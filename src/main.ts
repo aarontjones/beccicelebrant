c94e9bd17b8bb403203be9b41a0b922ffc447359
aarontjones <aarontjones4722@gmail.com>
2026-05-29 16:28:52 +0100

    added basic layout and TS structure

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+const app = document.getElementById("app")
+
+if (!app) throw new Error("App container not found")
+
+const container = document.createElement("div")
+container.className = "container"
+
+const mainContainer = document.createElement("div")
+mainContainer.className = "main-container"
+
+// Title
+const titleContainer = document.createElement("div")
+titleContainer.className = "title-container"
+
+const title = document.createElement("h2")
+title.className = "title"
+title.innerText = "Rebecca Roach"
+
+const tagline = document.createElement("p")
+tagline.className = "tagline"
+tagline.innerText = "celebrant"
+
+titleContainer.appendChild(title)
+titleContainer.appendChild(tagline)
+
+
+
+
+
+// Appending
+mainContainer.appendChild(titleContainer)
+
+container.appendChild(mainContainer)
+app.appendChild(container)
